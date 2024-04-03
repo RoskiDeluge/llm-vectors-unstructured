2751bc83ca579be06f941c5218e5a4b5d0810c9f
RoskiDeluge <roberto.delgado@gmail.com>
2024-04-02 22:38:20 -1000

    update

--- llm-vectors-unstructured/create_vector.py
+++ llm-vectors-unstructured/create_vector.py
@@ -1,20 +1,28 @@
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain.text_splitter import CharacterTextSplitter
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-from langchain_community.document_loaders import DirectoryLoader, TextLoader
 
 COURSES_PATH = "llm-vectors-unstructured/data/asciidoc"
 
 # Load lesson documents
-loader = DirectoryLoader(COURSES_PATH, glob="**/lesson.adoc", loader_cls=TextLoader)
+loader = DirectoryLoader(
+    COURSES_PATH, glob="**/lesson.adoc", loader_cls=TextLoader)
 docs = loader.load()
 
 # Create a text splitter
-# text_splitter =
+text_splitter = CharacterTextSplitter(
+    separator="\n\n",
+    chunk_size=1500,
+    chunk_overlap=200,
+)
 
 # Split documents into chunks
-# chunks =
+chunks = text_splitter.split_documents(docs)
+
+print(chunks)
 
 # Create a Neo4j vector store
 # neo4j_db =
